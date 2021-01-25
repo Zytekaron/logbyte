f9cec72113ba67587503eff59c066b6300fb9089
Zytekaron <zytekaron@gmail.com>
2021-01-25 03:43:44 -0800

    change server-side log entry type (logentry.go)

--- src/types/logentry.go
+++ src/types/logentry.go
@@ -24,21 +24,17 @@ type LogEntry struct {
 	// The message of the log entry
 	Message string `json:"message" bson:"message"`
 
-	// The data associated with this log entry, if any
-	Data map[string]interface{} `json:"data" bson:"data"`
-
 	// The time this log entry was created
 	CreatedAt int64 `json:"created_at" bson:"created_at"`
 }
 
 // Create a new LogEntry
-func NewLogEntry(id string, service string, level Level, message string, data map[string]interface{}, createdAt int64) *LogEntry {
+func NewLogEntry(id string, service string, level Level, message string, createdAt int64) *LogEntry {
 	return &LogEntry{
 		Id:        id,
 		Service:   service,
 		Level:     level,
 		Message:   message,
-		Data:      data,
 		CreatedAt: createdAt,
 	}
 }
